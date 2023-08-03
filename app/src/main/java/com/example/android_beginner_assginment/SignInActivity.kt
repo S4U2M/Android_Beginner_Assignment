f02a3683b21ab7ea3a4270607a1b449561f3b14e
송규진 <songu9610@gmail.com>
2023-08-03 12:19:53 +0900

    feat 로그인 데이터 넘기기 구현. 그러나 버그 존재

--- app/src/main/java/com/example/android_beginner_assginment/SignInActivity.kt
+++ app/src/main/java/com/example/android_beginner_assginment/SignInActivity.kt
@@ -1,6 +1,7 @@
 package com.example.android_beginner_assginment
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,23 +9,48 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 
 class SignInActivity : AppCompatActivity() {
+    private val id:EditText by lazy { findViewById(R.id.id_Edit) }
+    private val psw:EditText by lazy { findViewById(R.id.psw_Edit) }
+
+    private val setResultSignup = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val receivedData = result.data
+
+            if (receivedData != null) {
+                val receivedId = receivedData.getStringExtra("id")
+                val receivedpsw = receivedData.getStringExtra("psw")
+
+                if (receivedId != null) {
+                    id.setText(receivedId)
+                }
+
+                if (receivedpsw != null) {
+                    psw.setText(receivedpsw)
+                }
+            }
+        }
+    }
+
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign)
-        val signB = findViewById<Button>(R.id.sign)
+        val signButton = findViewById<Button>(R.id.sign)
 
-        signB.setOnClickListener {
+        signButton.setOnClickListener {
             val moveToSign = Intent(this, SignupActivity::class.java)
+            val intent = Intent(this,SignupActivity::class.java)
+            setResultSignup.launch(intent)
             startActivity(moveToSign)
         }
     }
 
     fun login(view: View) {
-        val id = findViewById<EditText>(R.id.id_Edit)
-        val psw = findViewById<EditText>(R.id.psw_Edit)
 
         val inputId = id.text.toString()
         val inputPsw = psw.text.toString()
--- app/src/main/java/com/example/android_beginner_assginment/SignupActivity.kt
+++ app/src/main/java/com/example/android_beginner_assginment/SignupActivity.kt
@@ -1,6 +1,8 @@
 package com.example.android_beginner_assginment
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -8,23 +10,24 @@ import android.text.TextWatcher
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 
 class SignupActivity : AppCompatActivity() {
-    private val id: EditText by lazy {findViewById(R.id.id_create) }
-    private val psw: EditText by lazy{findViewById(R.id.psw_create)}
-    private val name:EditText by lazy { findViewById(R.id.name_create) }
-    private val age:EditText by lazy { findViewById(R.id.age_create) }
-    private val gender:EditText by lazy { findViewById(R.id.gender_create) }
-    private val mbti:EditText by lazy { findViewById(R.id.mbti_create) }
-    private val hobby:EditText by lazy { findViewById(R.id.hobby_create) }
-
+    private val id: EditText by lazy { findViewById(R.id.id_create) }
+    private val psw: EditText by lazy { findViewById(R.id.psw_create) }
+    private val name: EditText by lazy { findViewById(R.id.name_create) }
+    private val age: EditText by lazy { findViewById(R.id.age_create) }
+    private val gender: EditText by lazy { findViewById(R.id.gender_create) }
+    private val mbti: EditText by lazy { findViewById(R.id.mbti_create) }
+    private val hobby: EditText by lazy { findViewById(R.id.hobby_create) }
 
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
 
         textInput(id)
         textInput(psw)
@@ -83,6 +86,11 @@ class SignupActivity : AppCompatActivity() {
                         inputhobby
                     )
                 )
+
+                val intent = Intent(this, SignInActivity::class.java)
+                intent.putExtra("id", inputId)
+                intent.putExtra("psw", inputpsw)
+                setResult(Activity.RESULT_OK, intent)
                 finish()
 
             } else {
@@ -163,15 +171,15 @@ class SignupActivity : AppCompatActivity() {
         id.setBackgroundColor(ContextCompat.getColor(this, android.R.color.white))
     }
 
-    fun textInput(id:EditText){
+    fun textInput(id: EditText) {
 
-        id.addTextChangedListener(object:TextWatcher{
+        id.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
 
             override fun afterTextChanged(s: Editable?) {
-                if (id.text.toString().isNotEmpty()){
+                if (id.text.toString().isNotEmpty()) {
                     beWhite(id)
                 }
             }
